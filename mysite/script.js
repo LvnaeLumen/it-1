71dbcca4226927bd4a4a0a0a1d1a0da53197732c
Ivan Erofeev <ierofeev@syntellect.ru>
2022-06-07 18:17:34 +0300

    Добавлена жабья кнопка

--- mysite/script.js
+++ mysite/script.js
@@ -1,4 +1,32 @@
 let page = document.querySelector('.page');
+
 let themeButton = document.querySelector('.theme-button');
 themeButton.onclick = function(){
-page.classList.toggle('dark-theme')};
+    page.classList.toggle('dark-theme')
+};
+
+
+let i = 0;
+var animations = ["files/frog-hat.gif",
+"files/frog-lily.gif",
+"files/frog-jumping.gif",
+"files/sapo-frog.gif",
+"files/spin-frog.gif",
+];
+
+
+let frogButton = document.querySelector('.frog-button');
+frogButton.onclick = function(){
+
+    image = document.querySelector('.about-image').src 
+    = animations[i];
+
+    if(i < 4)
+    {
+        i++;
+    }
+    else
+    {
+        i=0;
+    }
+};
